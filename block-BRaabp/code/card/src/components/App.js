46fdd190b083f6a769ab78fc382795a300c4491e
Reena Mendonsa <reenamendonsa@gmail.com>
2022-04-20 17:17:46 +0530

    completed

---
+++ block-BRaabp/code/card/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Fonts from "./Fonts";
+function App(props) {
+  return (
+    <React.Fragment>
+      <Fonts />
+    </React.Fragment>
+  );
+}
+export default App;
